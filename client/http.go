7eb0e532706d5a86665f65c0713a7da345002fdc
Michael Rodriguez <mike@recursivelabs.org>
2022-09-10 12:34:22 -0400

    add cache to base client

--- client/http.go
+++ client/http.go
@@ -4,12 +4,17 @@ import (
 	"bytes"
 	"io"
 	"net/http"
+	"sync/atomic"
+	"time"
+
+	gocache "github.com/mrod502/go-cache"
 )
 
 func NewHttp[T any](parser Parser[T]) *Http[T] {
 	return &Http[T]{
-		Client: http.DefaultClient,
-		parse:  parser,
+		Client:   http.DefaultClient,
+		parse:    parser,
+		useCache: &atomic.Bool{},
 	}
 }
 
@@ -23,14 +28,20 @@ func SetHeaders(r *http.Request) {
 
 type Http[T any] struct {
 	*http.Client
-	parse Parser[T]
+	parse    Parser[T]
+	useCache *atomic.Bool
+	cache    *gocache.Cache[*T, string]
 }
 
 func (h *Http[T]) Get(path string) (v *T, err error) {
+	return h.withCacheAttempt(path, func() (*T, error) {
+		return h.getBase(path)
+	})
+}
 
+func (h *Http[T]) getBase(path string) (v *T, err error) {
 	v = new(T)
 	req, _ := buildRequest(path, "GET", nil)
-
 	res, err := h.Do(req)
 	if err != nil {
 		return nil, err
@@ -38,6 +49,28 @@ func (h *Http[T]) Get(path string) (v *T, err error) {
 	err = h.parse(res.Body, v)
 	return
 }
+
+func (h *Http[T]) WithCache(lifetime time.Duration) *Http[T] {
+	h.useCache.Store(true)
+	h.cache = gocache.New[*T, string]().WithExpiration(lifetime)
+	return h
+}
+
+func (h *Http[T]) NoCache() *Http[T] {
+	h.useCache.Store(false)
+	h.cache = nil
+	return h
+}
+
+func (h *Http[T]) withCacheAttempt(key string, f func() (*T, error)) (*T, error) {
+	if h.useCache.Load() {
+		if h.cache.Exists(key) {
+			return h.cache.Get(key)
+		}
+	}
+	return f()
+}
+
 func buildRequest(uri, method string, body []byte) (*http.Request, error) {
 	var bod io.Reader
 	if body != nil {
