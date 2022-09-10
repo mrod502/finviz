1617862a9a31c50969d3f1c8c1040f94de897d77
Michael Rodriguez <mike@recursivelabs.org>
2022-09-10 15:03:30 -0400

    updated client

--- client.go
+++ client.go
@@ -1,50 +1,19 @@
 package finviz
 
 import (
-	"time"
-
 	"github.com/mrod502/finviz/home"
-	"github.com/mrod502/finviz/screener"
-	gocache "github.com/mrod502/go-cache"
+	"github.com/mrod502/finviz/news"
 )
 
 type Client struct {
-	endpoints *gocache.InterfaceCache
+	*home.HomeClient
+	*news.NewsClient
 }
 
-func (c *Client) Home() (h *home.Home, err error) {
-	if c.endpoints.Exists("home") {
-		v, err := c.endpoints.Get("home")
-		if err != nil {
-			return nil, err
-		}
-		return v.(*home.Home), nil
-	}
-	v, err := home.GetHome()
-	if err != nil {
-		return nil, err
-	}
-	c.endpoints.Set("home", v)
-	return v, nil
-}
-
-func (c *Client) News() {}
-
-func (c *Client) Screener() (*screener.Table, error) {
-	return &screener.Table{}, nil
-}
-
-func (c Client) Forex() {}
-
-func (c Client) Crypto() {}
-
-func NewClient(opts *Options) (c *Client, err error) {
-	if opts.cacheDuration < 0 {
-		opts.cacheDuration = 3 * time.Minute
-	}
+func New(opts *Options) (c *Client, err error) {
 	c = &Client{
-		endpoints: gocache.NewInterfaceCache().WithExpiration(opts.cacheDuration),
+		HomeClient: home.NewClient(opts.cacheDuration),
+		NewsClient: news.NewClient(opts.cacheDuration),
 	}
-
 	return
 }
